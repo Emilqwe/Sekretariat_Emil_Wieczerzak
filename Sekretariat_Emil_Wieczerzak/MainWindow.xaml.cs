803b51d84c448dcab12080118a2deb5e65e98488
Emilqwe <emomolo20@gmail.com>
2021-12-31 17:05:48 +0100

    Udoskonalanie funkcjonalności

--- Sekretariat_Emil_Wieczerzak/MainWindow.xaml.cs
+++ Sekretariat_Emil_Wieczerzak/MainWindow.xaml.cs
@@ -64,6 +64,38 @@ namespace Sekretariat_Emil_Wieczerzak
             }
         }
 
+        private void UczenK_Checked(object sender, RoutedEventArgs e)
+        {
+            if (UczenK.IsChecked == true)
+            {
+                WysPlecPrac.Content = WysImiePrac.Content + "\n" + UczenK.Content;
+            }
+        }
+
+        private void UczenM_Checked(object sender, RoutedEventArgs e)
+        {
+            if (UczenM.IsChecked == true)
+            {
+                WysPlecPrac.Content = WysImiePrac.Content + "\n" + UczenM.Content;
+            }
+        }
+
+        private void NauczycielK_Checked(object sender, RoutedEventArgs e)
+        {
+            if (NauczycielK.IsChecked == true)
+            {
+                WysPlecPrac.Content = WysImiePrac.Content + "\n" + NauczycielK.Content;
+            }
+        }
+
+        private void NauczycielM_Checked(object sender, RoutedEventArgs e)
+        {
+            if (NauczycielM.IsChecked == true)
+            {
+                WysPlecPrac.Content = WysImiePrac.Content + "\n" + NauczycielM.Content;
+            }
+        }
+
         private void PracownikPrzeslijDane_Click(object sender, RoutedEventArgs e)
         {
             WysImiePrac.Content = WysImiePrac.Content + "\n" + PracownikImie.Text;
@@ -79,6 +111,37 @@ namespace Sekretariat_Emil_Wieczerzak
             WysStanowisko.Content = WysStanowisko.Content + "\n" + PracownikStanowisko.SelectedItem;
             WysDataZatrPrac.Content = WysDataZatrPrac.Content + "\n" + PracownikDataZatr.SelectedDate;
 
+        }
+
+        private void UczenPrzeslijDane_Click(object sender, RoutedEventArgs e)
+        {
+            WysImieUcznia.Content = WysImieUcznia.Content + "\n" + UczenImie.Text;
+            WysDrugieImieUcznia.Content = WysDrugieImieUcznia.Content + "\n" + UczenDrugieImie.Text;
+            WysNazwUcznia.Content = WysNazwUcznia.Content + "\n" + UczenNazwisko.Text;
+            WysNazwPanUcznia.Content = WysNazwPanUcznia.Content + "\n" + UczenNazwiskoP.Text;
+            WysImieOjcaUcznia.Content = WysImieOjcaUcznia.Content + "\n" + UczenImieOjca.Text;
+            WysImieMatkiUcznia.Content = WysImieMatkiUcznia.Content + "\n" + UczenImieMatki.Text;
+            WysDataUroUcznia.Content = WysDataUroUcznia.Content + "\n" + UczenDataUro.SelectedDate;
+            WysPeselUcznia.Content = WysPeselUcznia.Content + "\n" + UczenPesel.Text;
+            WysKlasa.Content = WysKlasa.Content + "\n" + UczenKlasa.SelectedItem;
+            WysZdjUcznia.Content = WysZdjUcznia.Content + "\n" + UczenImie.Text;
+            WysZajPoz.Content = WysZajPoz.Content + "\n" + UczenZajPoz.SelectedItem;
+        }
+
+        private void NauczycielPrzeslijDane_Click(object sender, RoutedEventArgs e)
+        {
+            WysImieNaucz.Content = WysImieNaucz.Content + "\n" + NauczycielImie.Text;
+            WysDrugieImieNaucz.Content = WysDrugieImieNaucz.Content + "\n" + NauczycielDrugieImie.Text;
+            WysNazwNaucz.Content = WysNazwNaucz.Content + "\n" + NauczycielNazwisko.Text;
+            WysNazwPanNaucz.Content = WysNazwPanNaucz.Content + "\n" + NauczycielNazwiskoP.Text;
+            WysImieOjcaNaucz.Content = WysImieOjcaNaucz.Content + "\n" + NauczycielImieOjca.Text;
+            WysImieMatkiNaucz.Content = WysImieMatkiNaucz.Content + "\n" + NauczycielImieMatki.Text;
+            WysDataUroNaucz.Content = WysDataUroNaucz.Content + "\n" + NauczycielDataUro.SelectedDate;
+            WysPeselNaucz.Content = WysPeselNaucz.Content + "\n" + NauczycielPesel.Text;
+            WysWychow.Content = WysEtat.Content + "\n" + NauczycielWychow.SelectedItem;
+            WysZdjNaucz.Content = WysZdjNaucz.Content + "\n" + NauczycielImie.Text;
+            WysPrzedNaucz.Content = WysStanowisko.Content + "\n" + NauczycielPrzedNaucz.SelectedItem;
+            WysDataZatrNaucz.Content = WysDataZatrNaucz.Content + "\n" + NauczycielDataZatr.SelectedDate;
         }
     }
 }
